380254de8ff1ad089a442fef4bbd29ded7073a09
9oHigh <wkdska957@naver.com>
2022-08-11 23:43:14 +0900

    Fix: EmployeesViewController

---
+++ usket.Architectures/usket.Architectures/MVVM/View/EmployeesViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LoginViewController.swift
+//  usket.Architectures
+//
+//  Created by 이경후 on 2022/08/11.
+//
+
+import UIKit
+
+final class EmployeesViewController: BaseViewController, BaseInitializing {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setConfig()
+        setUI()
+        setConstraints()
+    }
+    
+    func setConfig() {
+    
+    }
+    
+    func setUI() {
+    
+    }
+    
+    func setConstraints() {
+    
+    }
+}
